228253c84cb71d5504adc375096d360ffc3fbdf1
luisFrames <luisvnapoles@gmail.com>
2023-08-12 15:52:06 -0400

    UploadPostView

--- instagram-clone/Core/TabBar/MainTabView.swift
+++ instagram-clone/Core/TabBar/MainTabView.swift
@@ -8,28 +8,47 @@
 import SwiftUI
 
 struct MainTabView: View {
+    
+    @State private var selectedIndex = 0
+    
     var body: some View {
-        TabView {
+        TabView(selection: $selectedIndex) {
             FeedView()
+                .onAppear {
+                    //quitar
+                    selectedIndex = 0
+                }
                 .tabItem {
                     Image(systemName: "house")
-                }
+                }.tag(0)
             SearchView()
+                .onAppear {
+                    selectedIndex = 1
+                }
                 .tabItem {
                     Image(systemName: "magnifyingglass")
+                }.tag(1)
+            UploadPostView(tabIndex: $selectedIndex)
+                .onAppear {
+                    selectedIndex = 2
                 }
-            Text("Feed")
                 .tabItem {
                     Image(systemName: "plus.square")
-                }
+                }.tag(2)
             Text("Feed")
+                .onAppear {
+                    selectedIndex = 3
+                }
                 .tabItem {
                     Image(systemName: "heart")
-                }
+                }.tag(3)
             CurrentProfileView(user: User.MockUsers[0])
+                .onAppear {
+                    selectedIndex = 4
+                }
                 .tabItem {
                     Image(systemName: "person")
-                }
+                }.tag(4)
         }
         .tint(.black)
     }
---
+++ instagram-clone/Core/UploadPost/UploadPostView.swift
@@ -0,0 +1,77 @@
+//
+//  UploadPostView.swift
+//  instagram-clone
+//
+//  Created by Luis Miguel Valdes Napoles on 8/11/23.
+//
+
+import SwiftUI
+import PhotosUI
+
+struct UploadPostView: View {
+    
+    @State private var caption = ""
+    @State private var imagePickerPresented = false
+    @StateObject var viewModel = UploadPostVM()
+    
+    @Binding var tabIndex: Int
+    
+    var body: some View {
+        VStack {
+            
+            // Action tool bar
+            HStack {
+                Button {
+                    caption = ""
+                    viewModel.postImage = nil
+                    viewModel.selectedImage = nil
+                    tabIndex = 0
+                } label: {
+                    Text("Cancel")
+                }
+                
+                Spacer()
+                
+                Text("New Post")
+                    .fontWeight(.semibold)
+                
+                Spacer()
+                
+                Button {
+                    print("Upload")
+                } label: {
+                    Text("Upload")
+                        .fontWeight(.semibold)
+                }
+            }
+            .padding(.horizontal)
+            
+            // Post image and Caption
+            HStack {
+                if let image = viewModel.postImage {
+                    image
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 100, height: 100)
+                        .clipped()
+                }
+                
+                TextField("Enter your caption", text: $caption, axis: .vertical)
+            }
+            .padding(.horizontal)
+            
+            Spacer()
+            
+        }
+        .onAppear {
+            imagePickerPresented.toggle()
+        }
+        .photosPicker(isPresented: $imagePickerPresented, selection:  $viewModel.selectedImage)
+    }
+}
+
+struct UploadPostView_Previews: PreviewProvider {
+    static var previews: some View {
+        UploadPostView(tabIndex: .constant(1))
+    }
+}
