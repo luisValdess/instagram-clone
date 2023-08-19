ac4c0719ced17e66436f13f2125bd929307e2593
luisFrames <luisvnapoles@gmail.com>
2023-08-19 16:52:31 -0400

    Authentication-Service

---
+++ instagram-clone/Core/Authentication/Services/AuthService.swift
@@ -0,0 +1,36 @@
+//
+//  AuthService.swift
+//  instagram-clone
+//
+//  Created by Luis Miguel Valdes Napoles on 8/19/23.
+//
+
+import Foundation
+import FirebaseAuth
+
+class AuthService {
+    
+    @Published var userSession: FirebaseAuth.User?
+    
+    static let shared = AuthService()
+    
+    init() {
+        self.userSession = Auth.auth().currentUser
+    }
+    
+    func login(withEmail email: String, password: String) async throws {
+        
+    }
+    
+    func createUser(email: String, password: String, username: String) async throws {
+        
+    }
+    
+    func loadUserData() async throws {
+        
+    }
+    
+    func signOut() {
+        
+    }
+}
---
+++ instagram-clone/Core/Root/ViewModel/ContentViewModel.swift
@@ -0,0 +1,30 @@
+//
+//  ContentViewModel.swift
+//  instagram-clone
+//
+//  Created by Luis Miguel Valdes Napoles on 8/19/23.
+//
+
+import Foundation
+import Firebase
+import Combine
+
+class ContentViewModel: ObservableObject {
+    
+    private let service = AuthService.shared
+    private var cancellables = Set<AnyCancellable>()
+    
+    @Published var userSession: FirebaseAuth.User?
+    
+    init() {
+        setupSubscribers()
+    }
+    
+    func setupSubscribers() {
+        service.$userSession.sink { [weak self] userSession in
+            self?.userSession = userSession
+        }
+        .store(in: &cancellables)
+    }
+       
+}
